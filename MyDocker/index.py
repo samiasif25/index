146cb44a7b76f1b2953f55a6684c5c22c08e4d37
Samiullah <samiasif25@gmail.com>
2021-10-07 11:20:49 +0530

    hello sami

--- MyDocker/index.py
+++ MyDocker/index.py
@@ -5,4 +5,7 @@ def hello():
     return "Hello World!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("9000"), debug=True)
-#     add stage3333
+#     add stage33335
+# I love travelling
+# HY Samii asif
+
